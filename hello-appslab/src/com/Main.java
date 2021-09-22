f5dcf37295a5a55962101b10bba3439ab46fe457
simon.kvasnovsky@gmail.com <simon.kvasnovsky@gmail.com>
2021-09-22 23:19:29 +0200

    a program that checks if it is a triangle

--- hello-appslab/src/com/Main.java
+++ hello-appslab/src/com/Main.java
@@ -1,8 +1,27 @@
 package com;
-
+import java.util.Scanner;
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello appslab 2020! haaa");
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = in.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = in.nextInt();
+        System.out.print("Enter third number: ");
+        int num3 = in.nextInt();
+
+        if (num1+num2>num3 && (num1+num3>num2) && (num3+num2>num1))
+        {
+            System.out.println("It is a triangle");
+        }
+        else
+        {
+            System.out.println("It is not a triangle");
+        }
     }
 }
+
+
