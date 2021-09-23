8b8c36f8450fe8035f6a9ef7f656545e70407458
z19ista16 <simon.kvasnovsky@gmail.com>
2021-09-23 09:18:57 +0200

    A program that checks if a word is palindrome

--- hello-appslab/src/com/Main.java
+++ hello-appslab/src/com/Main.java
@@ -2,7 +2,22 @@ package com;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello appslab 2020! haaa");
+    public static void main(String[] args)
+    {
+        String str = "Radar", reverseStr = "";
+
+        int strLength = str.length();
+
+        for (int i = (strLength - 1); i >=0; --i) {
+            reverseStr = reverseStr + str.charAt(i);
+        }
+
+        if (str.toLowerCase().equals(reverseStr.toLowerCase())) {
+            System.out.println(str + " is a Palindrome String.");
+        }
+        else {
+            System.out.println(str + " is not a Palindrome String.");
+        }
     }
+
 }
