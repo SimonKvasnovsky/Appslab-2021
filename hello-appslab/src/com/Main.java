d1e66da37610bbe4789a431f93f46e433400b909
simon.kvasnovsky@gmail.com <simon.kvasnovsky@gmail.com>
2021-09-25 22:43:58 +0200

    a program that prints smallest and biggest number

--- hello-appslab/src/com/Main.java
+++ hello-appslab/src/com/Main.java
@@ -2,7 +2,24 @@ package com;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello appslab 2020! haaa");
+    public static void main(String[] args)
+    {
+
+       int[] numbers =  {10, 52, 14, 98};
+       int smallest = numbers[0];
+       int largest = numbers[0];
+
+        for(int i=1; i< numbers.length; i++)
+        {
+            if(numbers[i] > largest)
+                largest = numbers[i];
+            else if (numbers[i] < smallest)
+                smallest = numbers[i];
+
+        }
+        System.out.println("Smallest Number is : " + smallest);
+        System.out.println("Largest Number is : " + largest);
     }
+
+
 }
