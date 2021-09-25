40574e8bac92b59461439409d749dbfef7ad1c27
simon.kvasnovsky@gmail.com <simon.kvasnovsky@gmail.com>
2021-09-25 20:40:39 +0200

    Game of rock, paper, scissors

--- hello-appslab/src/com/Main.java
+++ hello-appslab/src/com/Main.java
@@ -1,8 +1,53 @@
 package com;
+import java.util.Scanner;
 
-public class Main {
 
+class Main {
     public static void main(String[] args) {
-        System.out.println("Hello appslab 2020! haaa");
+
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Welcome to Rock, Paper, Scissors!");
+
+        while(true) {
+
+            System.out.print("Type rock, paper or scissors: ");
+            String myMove = in.nextLine();
+
+            if(myMove.equals("quit")) {
+                break;
+            }
+
+            if(!myMove.equals("rock") && !myMove.equals("paper") && !myMove.equals("scissors")) {
+
+                System.out.println("Your move is wrong, try rock,paper or scissors");
+
+            } else {
+
+
+                int rand = (int)(Math.random()*3);
+
+                String opponentMove = "";
+                if(rand == 0) {
+                    opponentMove = "rock";
+                } else if(rand == 1) {
+                    opponentMove = "paper";
+                } else {
+                    opponentMove = "scissors";
+                }
+                System.out.println("Opponent's move: " + opponentMove);
+
+                if(myMove.equals(opponentMove)) {
+                    System.out.println("It's a tie!");
+                } else if((myMove.equals("rock") && opponentMove.equals("scissors")) || (myMove.equals("scissors") && opponentMove.equals("paper")) || (myMove.equals("paper") && opponentMove.equals("rock"))) {
+                    System.out.println("You won!");
+                } else {
+                    System.out.println("You lost!");
+                }
+
+            }
+
+        }
+
     }
 }
