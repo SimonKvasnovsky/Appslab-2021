1c3fb2d30830b0f042d48abbd7ed3092686613dd
z19ista16 <simon.kvasnovsky@gmail.com>
2021-09-30 08:59:29 +0200

    Uncomplete CaptureTheRook exercise

--- hello-appslab/src/com/Main.java
+++ hello-appslab/src/com/Main.java
@@ -1,28 +1,27 @@
 package com;
 
+import java.util.Arrays;
+
 public class Main {
 
-    public static void main(String[] args)
-    {
-        String one = "abc";
-        boolean bc = one.contains("bc");
-        if(bc)
+
+    public static void main(String[] args) {
+        String[] a1 = new String[]{"A1"};
+        String[] a2 = new String[]{"B1"};
+        String[] a3 = new String[]{"A1"};
+        String[] a4 = new String[]{"C4"};
+        if (Arrays.equals(a1,a3))
         {
-            System.out.println(bc);;
+            System.out.println("They are both equal.");
         }
         else
         {
-            System.out.println(false);
+            System.out.println("false");
         }
+        boolean contains = Arrays.stream(a1).anyMatch("A"::equals) && Arrays.stream(a2).anyMatch("A"::equals) || Arrays.stream(a1).anyMatch("1"::equals) && Arrays.stream(a2).anyMatch("1"::equals);
 
-        
-        String two = "Car";
-        if(two.endsWith("ar"))
-        {
-            System.out.println(true);
-        }
-        else {
-            System.out.println(false);
-        }
+            System.out.println(contains);
+
+
     }
 }
